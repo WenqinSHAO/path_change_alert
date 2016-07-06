79e0819e6e2717c6cb0b17bcc5ebbc4f00ffbe4b
Wenqin SHAO <samuelshwq@gmail.com>
2016-07-06 16:29:48 +0200

    forget the homemade playback

---
+++ fastplayback.py
@@ -0,0 +1,28 @@
+from ripe.atlas.cousteau import AtlasResultsRequest
+from multiprocessing import Process
+import time
+
+
+class Fastplayback(Process):
+    def __init__(self, vis_q, analyze_q, query, interval=1):
+        self.id = (query['msm_id'], query['probe_ids'])
+        self.vis = vis_q
+        self.analyze = analyze_q
+        self.interval = interval
+        self.rec = []
+        is_success, results = AtlasResultsRequest(**query).create()
+        if is_success:
+            self.rec = results
+        else:
+            print "Result query failed!"
+
+        super(Fastplayback, self).__init__()
+
+    def run(self):
+        if self.rec:
+            for mes in self.rec:
+                self.vis.put(dict(id=self.id, type='mes', rec=mes))
+                self.analyze.put(dict(id=self.id, rec=mes))
+                time.sleep(self.interval)
+        self.analyze.put('STOP')
+        return
