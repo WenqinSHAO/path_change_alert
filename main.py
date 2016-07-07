24432d55a1beab8878ef54908b6ca5f52cc75865
Wenqin SHAO <samuelshwq@gmail.com>
2016-07-07 17:37:21 +0200

    slight tuning

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ from delayAnalyzer import DelayAnalyzer
 from multiprocessing import Queue, Manager
 from ripe.atlas.sagan import PingResult
 from bokeh.plotting import curdoc, figure
-from bokeh.models import ColumnDataSource, Slider
+from bokeh.models import ColumnDataSource, Slider, Div, HoverTool, Range1d
 from bokeh.layouts import row, column
 import Queue as NQ
+from os.path import dirname, join
+import datetime
+import pytz
+
+epoch = datetime.datetime.utcfromtimestamp(0)
+epoch = epoch.replace(tzinfo=pytz.UTC)
 
 
 def string_to_epoch(str):
@@ -25,9 +31,18 @@ def string_to_epoch(str):
     return int(calendar.timegm(time.strptime(str, '%d/%m/%Y %H:%M:%S')))
 
 
+def datetime_to_string(dt):
+    return datetime.datetime.strftime(dt, "%Y-%m-%d %H:%M:%S %Z")
+
+
+def datetime_to_epoch(dt):
+    return (dt-epoch).total_seconds()
+
+
 def update():
     analyze_setting['bias'] = bias.value
     analyze_setting['minlen'] = minlen.value
+
     try:
         task = vis_q.get(False)
     except NQ.Empty:
@@ -35,19 +50,23 @@ def update():
     else:
         if task != 'STOP':
             if task['type'] == 'base':
-                #print "BOKEH: Baseline update."
-                base.stream(task['rec'])
+                base.stream(dict(x=task['rec']['x'], y=task['rec']['y'], time=[datetime_to_string(i) for i in task['rec']['x']]))
             else:
                 one_mes = PingResult(task['rec'])
-                new_data = dict(x=[one_mes.created], y=[one_mes.rtt_min])
+                new_data = dict(x=[one_mes.created], y=[one_mes.rtt_min], time=[datetime_to_string(one_mes.created)])
                 if task['type'] == 'mes':
                     rec.append(one_mes.rtt_min)
+                    tstp.append(one_mes.created)
                     mes.stream(new_data)
+                    #if len(tstp) <= 15:
+                        #p.x_range = Range1d(datetime_to_epoch(tstp[0])*1000, datetime_to_epoch(tstp[0]+datetime.timedelta(seconds=3600*2))*1000)
+                    #    p.y_range = Range1d(200, 300)
+                    #else:
+                        #p.x_range = Range1d(datetime_to_epoch(tstp[-15])*1000, datetime_to_epoch(tstp[0]+datetime.timedelta(seconds=3600*2))*1000)
+                    #    p.y_range = Range1d(10, 20)
                 elif task['type'] == 'alert':
-                    #print "BOKEH: Alert received."
                     alert.stream(new_data)
                 elif task['type'] == 'loss':
-                    #print "BOKEH: Loss reported."
                     if rec:
                         new_data['y'] = [rec[-1]]
                     else:
@@ -76,14 +95,21 @@ for mes in mes_list:
     analyzer.start()
     mes_worker.append((streamer, analyzer))
 
-mes = ColumnDataSource(dict(x=[], y=[]))
-alert = ColumnDataSource(dict(x=[], y=[]))
-loss = ColumnDataSource(dict(x=[], y=[]))
-base = ColumnDataSource(dict(x=[], y=[]))
+mes = ColumnDataSource(dict(x=[], y=[], time=[]))
+alert = ColumnDataSource(dict(x=[], y=[], time=[]))
+loss = ColumnDataSource(dict(x=[], y=[], time=[]))
+base = ColumnDataSource(dict(x=[], y=[], time=[]))
 rec = []
+tstp = []
 
-p = figure(width=600, height=400, x_axis_type="datetime", title='Streaming demo',
+desc = Div(text=open(join(dirname(__file__), "des.html")).read(), width=800)
+
+hover = HoverTool(tooltips=[("RTT", "@y"), ("Time", "@time")])
+
+p = figure(width=600, height=400, x_axis_type="datetime", title="probe %d -- msm %d" % (mes_list[0][1], mes_list[0][0]),
            tools="xpan,xwheel_zoom,xbox_zoom,reset")
+p.add_tools(hover)
+
 p.border_fill_color = "whitesmoke"
 p.min_border_left = 80
 p.line(x='x', y='y', source=mes)
@@ -93,6 +119,9 @@ p.circle(x='x', y='y', source=loss, fill_color="grey", size=8, line_color='black
 p.line(x='x', y='y', source=base, line_color='green', line_width=3, alpha=0.5)
 p.xaxis.axis_label = 'Time'
 p.yaxis.axis_label = 'RTT (ms)'
+#p.x_range = Range1d(string_to_epoch('20/01/2016 06:00:00')*1000, string_to_epoch('25/01/2016 06:00:00')*1000)
+
+
 
 bias = Slider(title="RTT bias (ms)", value=10, start=0, end=50, step=1)
 minlen = Slider(title="Minimum data length", value=10, start=4, end=50, step=1)
@@ -100,9 +129,9 @@ analyze_setting['bias'] = bias.value
 analyze_setting['minlen'] = minlen.value
 
 doc = curdoc()
-doc.add_root(column(row(bias, minlen), p))
+doc.add_root(column(desc, row(bias, minlen), p))
 doc.add_periodic_callback(update, 500)
-doc.title = "Streaming Demo"
+doc.title = "RTT Streaming"
 
 #for st, ana in mes_worker:
 #    st.join()
